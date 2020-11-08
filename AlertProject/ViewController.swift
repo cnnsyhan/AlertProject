e99400ffb2180d01b7060c63ca32e016e7c3e87b
Can Seyhan <muhamedseyhan503@gmail.com>
2020-11-09 01:20:09 +0300

    Full Project

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    @IBOutlet weak var reEnterPassWordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        /*
+ 
+        let alert = UIAlertController(title: "Error!", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
+            // button clicked
+            print("button clicked")
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+ */
+        
+        if userNameTextField.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Username not found!")
+        } else if passWordTextField.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Password not found!")
+        } else if passWordTextField.text != reEnterPassWordTextField.text {
+            makeAlert(titleInput: "Error", messageInput: "Passwords do no match")
+        } else {
+           makeAlert(titleInput: "Success", messageInput: "User OK")
+        }
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput:String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
